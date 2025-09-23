e05755c3ee511827cba86dd948a2ff64d72fbc53
Your Name <you@example.com>
2025-09-23 00:28:09 -0500

    Fix imports for Vercel case sensitivity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,31 @@
 // src/App.jsx
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppShell from "./ui/AppShell";
 
-import Home from "./pages/Home.jsx";
+// Import pages (no .jsx extension, case-sensitive)
+import Home from "./pages/Home";
 import BreathingPage from "./pages/BreathingPage";
 import BilateralPage from "./pages/BilateralPage";
-import WorksheetsPage from "./pages/WorksheetsPage";
 import SafetyPlanPage from "./pages/SafetyPlanPage";
 import JournalPage from "./pages/JournalPage";
+import WorksheetsPage from "./pages/WorksheetsPage";
 
-export default function App() {
+function App() {
   return (
-    <AppShell>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/breathing" element={<BreathingPage />} />
-        <Route path="/bilateral" element={<BilateralPage />} />
-        <Route path="/worksheets" element={<WorksheetsPage />} />
-        <Route path="/safety" element={<SafetyPlanPage />} />
-        <Route path="/journal" element={<JournalPage />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </AppShell>
+    <Router>
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/breathing" element={<BreathingPage />} />
+          <Route path="/bilateral" element={<BilateralPage />} />
+          <Route path="/safety-plan" element={<SafetyPlanPage />} />
+          <Route path="/journal" element={<JournalPage />} />
+          <Route path="/worksheets" element={<WorksheetsPage />} />
+        </Routes>
+      </AppShell>
+    </Router>
   );
 }
+
+export default App;
